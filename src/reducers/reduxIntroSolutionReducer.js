b278371242258127f6df7c6b14f4d39cc25fffce
John McKeever <jmmckeever823@gmail.com>
2020-07-22 10:00:11 -0500

    removed useless console dirs.

--- src/reducers/reduxIntroSolutionReducer.js
+++ src/reducers/reduxIntroSolutionReducer.js
@@ -6,13 +6,11 @@ export default (state = initialState, action) => {
   console.dir(state)
   switch (action.type) {
     case 'SOLUTIONS_UPDATE_MESSAGE_TEXT':
-      console.dir('in expected action handler')
       return {
         ...state,
         messageText: action.newMessageText,
       }
     default:
-      console.dir('in default case')
       return state
   }
 }
