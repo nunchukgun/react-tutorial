9262cc6aef17cf5d6a6513f100a4a37f2f8954c8
John McKeever <jmmckeever823@gmail.com>
2020-08-10 13:29:21 -0500

    converted Home page to function component.

--- src/components/tableOfContents/Home.jsx
+++ src/components/tableOfContents/Home.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardContent from "@material-ui/core/CardContent";
-import Divider from "@material-ui/core/Divider";
-import Typography from "@material-ui/core/Typography";
-import ContentsList from "./ContentsList";
-import withStyles from "@material-ui/core/styles/withStyles";
+import React from 'react'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
+import Divider from '@material-ui/core/Divider'
+import Typography from '@material-ui/core/Typography'
+import ContentsList from './ContentsList'
+import withStyles from '@material-ui/core/styles/withStyles'
 
 const styles = (theme) => ({
   main: {
@@ -14,32 +14,30 @@ const styles = (theme) => ({
       flexGrow: 1,
       padding: theme.spacing(6),
     },
-    width: "100%",
-    height: "100%",
+    width: '100%',
+    height: '100%',
   },
   card: {
-    margin: "5% 15%",
-    textAlign: "center",
+    margin: '5% 15%',
+    textAlign: 'center',
   },
-});
+})
 
-class Home extends React.Component {
-  render = () => {
-    const { classes } = this.props;
+const Home = (props) => {
+  const { classes } = props
 
-    return (
-      <main className={classes.main}>
-        <Card className={classes.card}>
-          <CardHeader title={"React Tutorial"} subheader={"Let's learn!"} />
-          <CardContent>
-            <Typography variant={"h3"}>Table of Contents</Typography>
-            <Divider />
-            <ContentsList />
-          </CardContent>
-        </Card>
-      </main>
-    );
-  };
+  return (
+    <main className={classes.main}>
+      <Card className={classes.card}>
+        <CardHeader title={'React Tutorial'} subheader={"Let's learn!"} />
+        <CardContent>
+          <Typography variant={'h3'}>Table of Contents</Typography>
+          <Divider />
+          <ContentsList />
+        </CardContent>
+      </Card>
+    </main>
+  )
 }
 
-export default withStyles(styles)(Home);
+export default withStyles(styles)(Home)
